84287e8ffc11c10a65814d33edab81b340e2cdc0
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-19 14:58:24 -0700

    Update bank-console.sql

--- bank-console.sql
+++ bank-console.sql
@@ -1,14 +1,31 @@
 DROP TABLE IF EXISTS user_accounts;
+drop table if exists user_credentials;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS savings_accounts;
 DROP TABLE IF EXISTS checking_accounts;
+
 
 CREATE TABLE users(
 	id 				serial,
 	first_name 		varchar(25) not null,
 	last_name 		varchar(25) not null,
+	
+	constraint user_pk
+	primary key(id)
+);
+
+create table user_credentials(
+	id 				serial,
+	user_id 		int,
 	username 		varchar(25) unique not null, 
-	password 		varchar(100) not null
+	password 		varchar(100) not null,
+	
+	constraint cred_pk
+	primary key(id),
+	
+	constraint cred_fk
+	foreign key(user_id)
+	references users
 );
 
 commit;
