a2bbbfc5f250f3ada61257c1e5515b48bdca63b1
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-24 11:14:37 -0700

    Added methods in UserServicesTest:

--- src/test/java/com/revature/bankconsole/UserServicesTest.java
+++ src/test/java/com/revature/bankconsole/UserServicesTest.java
@@ -1,6 +1,64 @@
 package com.revature.bankconsole;
 
+import com.revature.bankconsole.models.UserInfo;
+import com.revature.bankconsole.repos.UserRepo;
+import com.revature.bankconsole.services.UserServices;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserServicesTest {
 
+    private UserServices sut;
+    private UserRepo mockUserRepo = Mockito.mock(UserRepo.class);
+    Set<UserInfo> mockUsers = new HashSet<>();
 
+    @Before
+    public void setup() {
+        sut = new UserServices(mockUserRepo);
+        mockUsers.add(new UserInfo(1, "Adam", "Inn", "admin", "p4ssw0rd", "admin@revbooks.com"));
+        mockUsers.add(new UserInfo(2, "Bob", "Barker", "bbk", "priceRight", "host@priceisright.com"));
+
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+        mockUsers.removeAll(mockUsers);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void authenticateWithInvalidCredentials() {
+
+        sut.authenticate("", "");
+
+    }
+
+    @Test
+    public void authenticateWithUnknownCredentials() {
+        UserInfo expectedUser = new UserInfo("Adam", "Inn", "admin", "p4ssw0rd");
+        Mockito.when(mockUserRepo.findUserByCredentials(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(java.util.Optional.of(expectedUser));
+
+        UserInfo actualResult = sut.authenticate("admin", "password");
+
+        Assert.assertEquals(expectedUser, actualResult);
+    }
+
+    @Test
+    public void authenticateWithValidCredentials() {
+        UserInfo expectedUser = new UserInfo("Adam", "Inn", "admin", "p4ssw0rd");
+        Mockito.when(mockUserRepo.findUserByCredentials(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(java.util.Optional.of(expectedUser));
+
+        UserInfo actualResult = sut.authenticate("admin", "password");
+
+        Assert.assertEquals(expectedUser, actualResult);
+
+    }
 }
